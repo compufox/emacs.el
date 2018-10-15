07fa4418e5ea0a830f2306d52f2e4f5c5e008107
Zac <thezacattacks@gmail.com>
2018-10-15 12:57:56 -0400

    updated packages
    Former-commit-id: da1f88ee546d01ef0b9a513a973242013fa5bd55

--- emacs.d/elpa/irony-20181013.1454/irony-pkg.el
+++ emacs.d/elpa/irony-20181013.1454/irony-pkg.el
@@ -1,4 +1,4 @@
-(define-package "irony" "20180703.1740" "C/C++ minor mode powered by libclang"
+(define-package "irony" "20181013.1454" "C/C++ minor mode powered by libclang"
   '((cl-lib "0.5")
     (json "1.2"))
   :keywords
