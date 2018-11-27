8c32d41a99dd269104aca6adf686402bc3ec4546
Zac <thezacattacks@gmail.com>
2018-11-26 23:53:39 -0500

    updated packages
    Former-commit-id: 65f7ace87d29e1c57d068e36497f83b683571a74

--- emacs.d/elpa/swiper-20181118.1535/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20181118.1535" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "c1c1ec7cb9c99c50424416a143747f64e3083552" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
