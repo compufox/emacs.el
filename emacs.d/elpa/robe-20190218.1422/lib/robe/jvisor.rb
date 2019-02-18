c45c7f5ea4bf42a7f291985ca515f49d52badba8
Zac <thezacattacks@gmail.com>
2019-02-18 15:09:31 -0500

    updated packages
    Former-commit-id: 0109923840d67d4313a644f79152a7b1acf0c328

---
+++ emacs.d/elpa/robe-20190218.1422/lib/robe/jvisor.rb
@@ -0,0 +1,14 @@
+require 'robe/visor'
+
+module Robe
+  class JVisor < Visor
+    def each_object(mod)
+      # JRuby has weird Module's (Java package objects, etc)
+      ObjectSpace.each_object(mod).select { |m| m.respond_to? :instance_methods }
+    end
+
+    def descendants(cls)
+      ObjectSpace.each_object(Class).select { |c| c < cls }
+    end
+  end
+end
