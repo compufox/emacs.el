ff3f509a1230639e2c256ae50522d0fa070e1b72
Zac <thezacattacks@gmail.com>
2019-02-20 17:34:25 -0500

    updated packages
    Former-commit-id: 63eaf3237ea9cbb6798ff50ef5f3e5a7c8146f3f

--- emacs.d/elpa/inf-ruby-20190130.1335/inf-ruby-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "inf-ruby" "20190130.1335" "Run a Ruby process in a buffer" 'nil :commit "5531aaeb89602e04ecf749a20d9126fb0a673ac3" :keywords '("languages" "ruby") :authors '(("Yukihiro Matsumoto") ("Nobuyoshi Nakada") ("Cornelius Mika" . "cornelius.mika@gmail.com") ("Dmitry Gutov" . "dgutov@yandex.ru") ("Kyle Hargraves" . "pd@krh.me")) :maintainer '("Yukihiro Matsumoto") :url "http://github.com/nonsequitur/inf-ruby")
