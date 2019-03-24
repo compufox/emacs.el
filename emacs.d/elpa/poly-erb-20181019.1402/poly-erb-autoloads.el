cac609da672352c36b488812665e1a2e743ebd3c
Zac <thezacattacks@gmail.com>
2019-03-24 01:02:55 -0400

    updated packages
    Former-commit-id: 9fe996a3d33f573d2b4e36eea49fa2a832b03f68

--- emacs.d/elpa/poly-erb-20181019.1402/poly-erb-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; poly-erb-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "poly-erb" "poly-erb.el" (0 0 0 0))
-;;; Generated autoloads from poly-erb.el
- (autoload 'poly-coffee+erb-mode "poly-erb")
-
-(define-polymode poly-js+erb-mode :hostmode 'pm-host/js :innermodes '(pm-inner/erb))
- (autoload 'poly-html+erb-mode "poly-erb")
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "poly-erb" '("pm-inner/erb" "poly-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; poly-erb-autoloads.el ends here
---
+++ emacs.d/elpa/poly-erb-20190317.1423/poly-erb-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "poly-erb" "20190317.1423" "Polymode for erb" '((emacs "25") (polymode "0.1.5")) :commit "5339b0ba665cbf62dc09571e9fbc5e824c5bf5e4" :keywords '("emacs") :authors '(("Siavash Sajjadi and Vitalie Spinu")) :maintainer '("Vitalie Spinu") :url "https://github.com/polymode/poly-erb")
