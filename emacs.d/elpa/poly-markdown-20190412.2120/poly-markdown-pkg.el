f100e15cb5ab2bff88fd8e6345a7e8b455687eaa
Zac <thezacattacks@gmail.com>
2019-04-17 13:14:44 -0400

    updated packages
    Former-commit-id: 4e05aa7654038c4fc84d0994acdda416370a38ab

--- emacs.d/elpa/poly-markdown-20190412.2120/poly-markdown-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "poly-markdown" "20190412.2120" "Polymode for markdown-mode" '((emacs "25") (polymode "0.1.5") (markdown-mode "2.3")) :commit "92be7596605d3770e8c5a1647e0e024d76579e80" :keywords '("emacs") :authors '(("Vitalie Spinu")) :maintainer '("Vitalie Spinu") :url "https://github.com/polymode/poly-markdown")
