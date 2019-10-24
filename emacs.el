617eb3882c55f55ebb43ea25becc280479d212a0
Zac <thezacattacks@gmail.com>
2019-10-24 09:00:42 -0400

    added a keystroke way to open macrostepper
    Former-commit-id: 458a7313ee2cfe4eb43a323c331a03f018ab5e71

--- emacs.el
+++ emacs.el
@@ -266,6 +266,13 @@ TYPE-NAMES is either a single string or a list of strings which represent the sy
 (add-hook 'c++-mode-hook (lambda ()
 			  (local-set-key (kbd "C-c i") 'init-cpp-file)))
 
+;; sets up a keystroke way to do macrostepper
+(add-hook 'slime-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c e") 'macrostep-expand)))
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c e") 'macrostep-expand)))
 
 ;; sets up my custom key bindings
 (global-set-key (kbd "C-x M-f") 'horz-flip-buffers)
