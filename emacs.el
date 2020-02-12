9c40cfd5b5e17d3fbb5cf9a5ee05d43eb0a93cd8
ava <dev@computerfox.xyz>
2020-02-12 09:36:21 -0500

    removed parinfer mode from executing automatically
    Former-commit-id: b190ef051c05c7e06503aa33ce2365f80f4c6281

--- emacs.el
+++ emacs.el
@@ -201,28 +201,27 @@ TYPE-NAMES is a list of strings that correspond to values returned by system-typ
 				   telephone-line-buffer-segment))))
 (telephone-line-mode t)
 
-(use-package parinfer
-  :ensure t
-  :bind
-  (("C-," . parinfer-toggle-mode))
-  :init
-  (progn
-    (setq parinfer-extensions
-          '(defaults       ; should be included.
-             pretty-parens  ; different paren styles for different modes.
-             paredit        ; Introduce some paredit commands.
-             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-             smart-yank))   ; Yank behavior depend on mode.
-    (add-hook 'clojure-mode-hook #'parinfer-mode)
-    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'scheme-mode-hook #'parinfer-mode)
-    (add-hook 'lisp-mode-hook #'parinfer-mode)))
-
 (use-package rainbow-delimiters
   :hook ((lisp-mode . rainbow-delimiters-mode)
 	 (emacs-lisp-mode . rainbow-delimiters-mode)
 	 (sly-mode . rainbow-delimiters-mode)))
+
+;(use-package parinfer
+;  :ensure t
+;  :bind
+;  (("C-," . parinfer-toggle-mode))
+;  :init
+;  (progn
+;    (setq parinfer-extensions
+;          '(defaults       ; should be included.
+;             pretty-parens  ; different paren styles for different modes.
+;             paredit        ; Introduce some paredit commands.
+;             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+;             smart-yank   ; Yank behavior depend on mode.
+;    (add-hook 'clojure-mode-hook #'parinfer-mode)
+;    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+;    (add-hook 'scheme-mode-hook #'parinfer-mode)))
+
 
 (use-package yasnippet
   :bind ("C-c s" . yas-insert-snippet))
