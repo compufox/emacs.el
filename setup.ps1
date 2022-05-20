9d4dcf58016caf9f9ed199e1d64919267d8fa390
Ava Fox <dev@computerfox.xyz>
2022-05-20 12:27:32 -0400

    supersceded setup.bat with setup.ps1
    added some output into setup.fish/bash
    
    
    Former-commit-id: 6a2dfd3d2102385282dec5ed00323a876e941ec4

---
+++ setup.ps1
@@ -0,0 +1,32 @@
+# Since windows won't let us make symlinks unless we're running
+#  as admin, we check here and then restart if needed
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Start-Process -FilePath PowerShell.exe -ArgumentList $myinvocation.mycommand.definition -Verb runAs
+    Break
+}
+
+If (Test-Path -Path $env:appdata\.emacs -PathType Leaf)
+{
+    mv $env:appdata\.emacs $env:appdata\.emacs.bak
+}
+
+If (Test-Path -Path $env:appdata\.emacs.d -PathType Container)
+{
+    mv $env:appdata\.emacs.d $env:appdata\.emacs.d.bak
+}
+
+echo "Making link..."
+New-Item -ItemType SymbolicLink -Path $env:appdata\.emacs -Target $PSScriptRoot\emacs.el
+
+echo "Installing fonts..."
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+foreach ($file in gci $PSScriptRoot\Fonts\*.otf)
+{
+    $fileName = $file.Name
+    if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
+        echo "installing $fileName"
+        dir $file | %{ $fonts.CopyHere($_.fullname) }
+    }
+}
+cp *.otf c:\windows\fonts\
