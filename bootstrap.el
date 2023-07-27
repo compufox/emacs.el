225562746f570b5e471c354ed8086e96aeb617b5
a. fox <dev@computerfox.xyz>
2023-07-26 22:19:10 -0400

    byte compile our tangled file during bootstrap process

--- bootstrap.el
+++ bootstrap.el
@@ -24,6 +24,7 @@
                            (rx string-start
                                (or "emacs-lisp" "emacs")
                                string-end))
+    (byte-compile-file tangled-file)
     (load tangled-file nil 'no-message)))
 
 ;; then we iterate thru our list of config files and
