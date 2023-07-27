5fcab3c82583ccad705495e897f88ad63c9322a5
a. fox <dev@computerfox.xyz>
2023-07-27 11:09:19 -0400

    cleaned up setup scripts to use names based on operating system
    deleted fish setup script since its features are duplicated in bash setup

--- linux_setup.bash
+++ linux_setup.bash
@@ -18,9 +18,10 @@ emacs --script "tangle-bootstrap.el"
 echo "Making link..."
 ln -sf $(pwd)/bootstrap.el ~/.emacs
 
-echo "Installing git hooks..."
-cp hooks/* .git/hooks/
-chmod +x .git/hooks/*
+## Commenting this out for now
+#echo "Installing git hooks..."
+#cp hooks/* .git/hooks/
+#chmod +x .git/hooks/*
 
 echo "Installing fonts..."
 find -E fonts -type f -regex ".*\.(otf|ttf)" -exec cp \{\} $font_dir \;
---
+++ macos_setup.command
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+CONFIG_DIR=$(dirname $0)
+
+cd $CONFIG_DIR
+
+/usr/bin/env bash linux_setup.bash
