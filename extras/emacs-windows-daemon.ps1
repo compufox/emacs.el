4aa0d7b8481bb2d3645e3e31d092129cbb722954
a. fox <dev@computerfox.xyz>
2024-09-18 13:16:54 -0400

    tweaked config for better daemon mode support
    renamed windows service file

---
+++ extras/emacs-windows-daemon.ps1
@@ -0,0 +1,2 @@
+$EMACS_SERVER_FILE = "$HOME\AppData\Roaming\.emacs.d\server\server"
+C:\"Program Files"\Emacs\emacs-29.4\bin\runemacs.exe --daemon --chdir $HOME
--- extras/windows_daemon.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Emacs\emacs-29.4\bin\emacs.exe --daemon"
