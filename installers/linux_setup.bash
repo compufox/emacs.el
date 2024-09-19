f8725fba1982910786e7f6fc83ea699ed4784a31
a. fox <dev@computerfox.xyz>
2024-09-19 16:58:37 -0400

    updated install scripts to offer to install daemon services

--- installers/linux_setup.bash
+++ installers/linux_setup.bash
@@ -1,20 +1,26 @@
 #!/bin/env bash
-font_dir = "~/.fonts"
+FONT_DIR="~/.fonts"
+SERVICE_FILE="emacs-user-systemd.service"
+SERVICE_LOCATION="~/.config/systemd/user"
+SERVICE_ACTIVATION_README="Activate service by running: systemd --user enable $SERVICE_FILE"
 
 if [ "$(basename $PWD)" == "installers" ] ; then
     cd ..
 fi
 
 if [ "$(uname)" == "Darwin" ] ; then
-    font_dir = "~/Library/Fonts"
+    FONT_DIR = "~/Library/Fonts"
+    SERVICE_FILE = "emacs-macos-launchagent.plist"
+    SERVICE_LOCATION = "~/Library/LaunchAgents"
+    SERVICE_ACTIVATION_README = "Activate service by running: launchctl bootup $SERVICE_LOCATION/$SERVICE_FILE"
 fi
 
-if [ ! -d $font_dir ] ; then
-    mkdir $font_dir
+if [ ! -d $FONT_DIR ] ; then
+    mkdir $FONT_DIR
 fi
 
-[[ -f ~/.emacs ]] ; mv ~/.emacs ~/.emacs.bak
-[[ -d ~/.emacs.d ]] ; mv ~/.emacs.d ~/.emacs.d.bak
+[[ -f ~/.emacs ]] && mv ~/.emacs ~/.emacs.bak
+[[ -d ~/.emacs.d ]] && mv ~/.emacs.d ~/.emacs.d.bak
 
 echo "Making link..."
 ln -sf $(pwd)/bootstrap.el ~/.emacs
@@ -25,8 +31,18 @@ ln -sf $(pwd)/bootstrap.el ~/.emacs
 #chmod +x .git/hooks/*
 
 echo "Installing fonts..."
-find -E fonts -type f -regex ".*\.(otf|ttf)" -exec cp \{\} $font_dir \;
+find -E fonts -type f -regex ".*\.(otf|ttf)" -exec cp \{\} $FONT_DIR \;
 fc-cache -v 
 
 echo "Touching local.el file"
 touch local.el
+
+echo "Install service to run Emacs as a daemon on login? (y/N) "
+read response
+
+if [ "$response" == "y" ] ; then
+    mkdir -p $SERVICE_LOCATION
+    ln -sf $(pwd)/extras/"$SERVICE_FILE" "$SERVICE_LOCATION/$SERVICE_FILE"
+
+    echo $SERVICE_ACTIVATION_README
+fi
--- installers/windows_setup.ps1
+++ installers/windows_setup.ps1
@@ -6,14 +6,6 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
-
-# the script may be running from the installers directory.
-# if it is then we need to go up a directory to ensure that
-# our PWD is set as the rest of the script expects
-$current_dir = (Split-Path -Path $PWD -Leaf)
-
-If ($current_dir -eq "installers") { cd .. }
-
 If (Test-Path -Path $env:appdata\.emacs -PathType Leaf)
 {
     mv $env:appdata\.emacs $env:appdata\.emacs.bak
@@ -25,11 +17,11 @@ If (Test-Path -Path $env:appdata\.emacs.d -PathType Container)
 }
 
 echo "Making link..."
-New-Item -ItemType SymbolicLink -Path $env:appdata\.emacs -Target $PSScriptRoot\bootstrap.el
+New-Item -ItemType SymbolicLink -Path $env:appdata\.emacs -Target $PSScriptRoot\..\bootstrap.el
 
 echo "Installing fonts..."
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
-foreach ($file in gci $PSScriptRoot\fonts\*.otf)
+foreach ($file in gci $PSScriptRoot\..\fonts\*.otf)
 {
     $fileName = $file.Name
     if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
@@ -38,4 +30,9 @@ foreach ($file in gci $PSScriptRoot\fonts\*.otf)
     }
 }
 
-New-Item -ItemType File -Path $PSScriptRoot\local.el 
+New-Item -ItemType File -Path $PSScriptRoot\..\local.el 
+
+$response = Read-Host -Prompt "Install service to run Emacs as a daemon when you log in? (y/N) "
+if ($response -eq "y*") {
+    New-Item -ItemType SymbolicLink -Path $env:appdata\Microsoft\Windows\"Start Menu"\Programs\Startup\emacs-windows-daemon.ps1 -Target $PSScriptRoot\..\extras\emacs-windows-daemon.ps1
+}
