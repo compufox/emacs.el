3fe23f13928a9d3812be850b6ed12024a5370f53
a. fox <dev@computerfox.xyz>
2025-02-25 10:37:26 -0500

    updated custom-vars form to fix issue with emacs 30 on windows
    - updated windows daemon script to reference emacs-30.1

--- extras/emacs-windows-daemon.ps1
+++ extras/emacs-windows-daemon.ps1
@@ -1,2 +1,2 @@
 $EMACS_SERVER_FILE = "$HOME\AppData\Roaming\.emacs.d\server\server"
-C:\"Program Files"\Emacs\emacs-29.4\bin\runemacs.exe --daemon --chdir $HOME
+C:\"Program Files"\Emacs\emacs-30.1\bin\runemacs.exe --daemon --chdir $HOME
