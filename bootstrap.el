f77f9d46b80a0d3e8bf0ac631c6464326f2a9b8a
a. fox <dev@computerfox.xyz>
2025-02-26 10:40:55 -0500

    updated bootstrap code to ensure correct path is set on macOS

--- bootstrap.el
+++ bootstrap.el
@@ -1,3 +1,24 @@
+;; ensure that our PATH is set correctly if we're not on windows
+(when (equal system-type 'darwin)
+  (setenv "PATH"
+          (concat (expand-file-name "~/bin") ":"
+                  (expand-file-name "~/.roswell/bin") ":"
+                  "/opt/homebrew/sbin:"
+                  "/opt/homebrew/bin:"
+                  "/usr/local/bin:"
+                  "/System/Cryptexes/App/usr/bin:"
+                  "/usr/bin:"
+                  "/bin:"
+                  "/usr/sbin:"
+                  "/sbin:"
+                  "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:"
+                  "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:"
+                  "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:"
+                  "/Library/Apple/usr/bin:"
+                  "/usr/local/MacGPG2/bin:"
+                  "/Applications/iTerm.app/Contents/Resources/utilities"))
+  (setq exec-path (string-split (getenv "PATH") path-separator)))
+
 (let ((strap-file (file-name-concat user-emacs-directory "init" "strapped.el")))
   ;; checks for our tangled bootstrap elisp file
   ;; if it doesnt exist, we create the emacs.d/init directory
